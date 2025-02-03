b548fe72fd89b113d46d4675346eb6e02a7e055f
svenickx <sven.dockx@gmail.com>
2025-02-03 18:29:54 +0100

    update audios

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,6 @@ import AuthService from './services/auth.service';
 
 import swaggerUi from 'swagger-ui-express';
 import {migrateToLatest} from './config/db/db';
-import {isTest} from './config/env';
 import AlbumController from './controllers/album.controller';
 import ArtistController from './controllers/artist.controller';
 import CategoryController from './controllers/category.controller';
@@ -72,9 +71,9 @@ app.use(cookieParser());
 app.use(measureRequestTime.get);
 // app.use(limiter.global);
 
-if (!isTest) {
-  migrateToLatest();
-}
+// if (!isTest) {
+migrateToLatest();
+// }
 
 //#region Repositories
 const logRepository = new LogRepository();
